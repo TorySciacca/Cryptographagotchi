7f1e3a3dceb861a00b24b879a208c59429c6ae02
TorySciacca <57762089+TorySciacca@users.noreply.github.com>
2024-06-18 20:53:23 +1000

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function button_a() {
+    console.log('select');
+    document.getElementById("debug").innerText = 'select'
+
+};
+
+function button_b() {
+    console.log('execute');
+    document.getElementById("debug").innerText = 'execute'
+
+};
+
+function button_c() {
+    console.log('cancel');
+    document.getElementById("debug").innerText = 'cancel'
+};
