e30a77991110b26c446a33ae34d66af93c8e333d
TorySciacca <57762089+TorySciacca@users.noreply.github.com>
2024-06-18 21:33:48 +1000

    added debug mode
    debug mode is only accessible via the console

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
 
-function button_a() {
-    console.log('select');
-    document.getElementById("debug").innerText = 'select'
+var debug = true //bool to determine if debug mode is on/off
 
+function set_debug(set_debug) {
+    debug = set_debug
+
+    if (debug) {
+        document.getElementById("debug").innerText = 'debug on';
+    } else {
+        document.getElementById("debug").innerText = ' ';
+    }
+};
+
+function button_a() {
+    if (debug == true){
+        console.log('select');
+        document.getElementById("debug").innerText = 'select'
+        }
 };
 
 function button_b() {
-    console.log('execute');
-    document.getElementById("debug").innerText = 'execute'
-
+    if (debug == true){
+        console.log('execute');
+        document.getElementById("debug").innerText = 'execute'
+        }
 };
 
 function button_c() {
-    console.log('cancel');
-    document.getElementById("debug").innerText = 'cancel'
+    if (debug == true){
+        console.log('cancel');
+        document.getElementById("debug").innerText = 'cancel'
+        }
 };
+
+//initate debug
+set_debug(false)
