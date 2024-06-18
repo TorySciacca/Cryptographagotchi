b55b0a87a2eb36de0106719be851c4fe9ddd210f
TorySciacca <57762089+TorySciacca@users.noreply.github.com>
2024-06-18 22:28:53 +1000

    add debug/keypress initiate functions

--- main.js
+++ main.js
@@ -11,6 +11,17 @@ function set_debug(set_debug) {
     }
 };
 
+function init_debug(){
+    if(window.location.href.indexOf("GitHub")) // If the URL is in a github folder location, then user is a dev
+        {
+            set_debug(true)
+        }
+    else
+        {
+            set_debug(false)
+        }
+    };
+
 function button_a() {
     if (debug == true){
         console.log('select');
@@ -32,5 +43,21 @@ function button_c() {
         }
 };
 
-//initate debug
-set_debug(false)
+function map_keyboard_shortcuts() { //alternative way of interacting with UI
+    window.addEventListener('keydown',(event) => {
+        if (event.key == '1'){
+            button_a()
+        } else if (event.key == '2'){
+            button_b()
+        } else if (event.key == '3'){
+            button_c()
+        }
+    })
+}
+
+function on_start() {
+    init_debug()
+    map_keyboard_shortcuts() 
+}
+
+on_start()
