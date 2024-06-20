8c8f38cbff5e308ad4464bbc442e49bdbbe68f8e
TorySciacca <57762089+TorySciacca@users.noreply.github.com>
2024-06-20 20:43:36 +1000

    refactor button ui function

--- main.js
+++ main.js
@@ -18,29 +18,7 @@ function init_debug(){
         { set_debug(false)}
     };
 
-function button_a() {
-    highlightOnLeftClick('a')
-    if (debug == true){
-        console.log('select');
-        document.getElementById("debug").innerText = 'select'
-        }
-};
-
-function button_b() {
-    highlightOnLeftClick('b')
-    if (debug == true){
-        console.log('execute');
-        document.getElementById("debug").innerText = 'execute'
-        }
-};
-
-function button_c() {
-    highlightOnLeftClick('c')
-    if (debug == true){
-        console.log('cancel');
-        document.getElementById("debug").innerText = 'cancel'
-        }
-};
+// init and mapping functions
 
 function map_keyboard_shortcuts() { //alternative way of interacting with UI
     window.addEventListener('keydown',(event) => {
@@ -54,8 +32,8 @@ function map_keyboard_shortcuts() { //alternative way of interacting with UI
             button_c()
             document.getElementById('c').style.backgroundColor = '#f1f8d4ff';
         }
-    })
-    
+    });
+
     window.addEventListener('keyup',(event) => {
         if (event.key == '1'){
             document.getElementById('a').style.backgroundColor = '#241f21ff';
@@ -64,7 +42,17 @@ function map_keyboard_shortcuts() { //alternative way of interacting with UI
         } else if (event.key == '3'){
             document.getElementById('c').style.backgroundColor = '#241f21ff';
         }
-    })
+    });
+
+    window.addEventListener('mousedown',(event) => {
+        if (event.target == document.getElementById('a')){
+            button_a()
+        } else if (event.target == document.getElementById('b')) {
+            button_b()
+        } else if (event.target == document.getElementById('c')) {
+            button_c()
+        }
+    });
 
     window.addEventListener('mouseup',(event) => {
         if (event.buttons == 0){
@@ -74,22 +62,36 @@ function map_keyboard_shortcuts() { //alternative way of interacting with UI
     })
 }
 
-function highlightOnLeftClick(IDString) {
-    const element = document.getElementById(IDString);
-    
-    element.addEventListener('mousedown', function(event) {
-        // Check if the left mouse button (button code 0) is clicked
-        if (event.button === 0) {
-            // Apply the highlight style
-            element.style.backgroundColor = '#f1f8d4ff';
-        }
-    });
-}
-
-
 function on_start() {
     init_debug()
     map_keyboard_shortcuts() 
+};
+
+
+//
+
+function button_a() {
+    document.getElementById('a').style.backgroundColor = '#f1f8d4ff'
+    if (debug == true){
+        console.log('select');
+        document.getElementById("debug").innerText = 'select'
+        }
+};
+
+function button_b() {
+     document.getElementById('b').style.backgroundColor = '#f1f8d4ff'
+    if (debug == true){
+        console.log('execute');
+        document.getElementById("debug").innerText = 'execute'
+        }
+};
+
+function button_c() {
+     document.getElementById('c').style.backgroundColor = '#f1f8d4ff'
+    if (debug == true){
+        console.log('cancel');
+        document.getElementById("debug").innerText = 'cancel'
+        }
 };
 
 on_start()
